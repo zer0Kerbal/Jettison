354d63a5d8502f6edcc5b5dfe6c9b0beeb63f5e2
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-02-08 23:00:25 -0600

    1.3.0.0

---
+++ Source/Class1.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using KSP;
+
+namespace Jettison
+{
+    public class Jettison : PartModule
+    {
+        public override void OnUpdate()
+        {
+            int count = 0;
+
+            foreach (PartResource resource in part.Resources)
+            {
+                if (resource.resourceName == "ElectricCharge")
+                {
+                    continue;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                foreach (BaseEvent ev in Events)
+                {
+                    if (ev.guiName == "Jettison resource")
+                    {
+                        ev.active = false;
+                    }
+                }
+
+                base.OnUpdate();
+            }
+        }
+
+        [KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Jettison Resource")]
+        public void button_JettisonClicked()
+        {
+            PartResource liquidFuel = null;
+            bool emptiedSomething = false;
+
+            foreach (PartResource resource in part.Resources)
+            {
+                if (!resource.flowState || resource.resourceName == "ElectricCharge")
+                {
+                    continue;
+                }
+
+                if (resource.resourceName == "LiquidFuel")
+                {
+                    liquidFuel = resource;
+                }
+                else
+                {
+                    if (resource.amount > 0.01f)
+                    {
+                        resource.amount = 0f;
+                        emptiedSomething = true;
+                        continue;
+                    }
+                }
+            }
+
+            if (!emptiedSomething && liquidFuel != null)
+            {
+                liquidFuel.amount = 0f;
+            }
+        }
+    }
+}
